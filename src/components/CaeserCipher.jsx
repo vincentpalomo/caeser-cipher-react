2a6add8d48377fd6b5bd2ebb8708a608b3197e5a
vincent <vincentpalomo@yahoo.com>
2023-01-01 18:39:54 -0600

    finished js for caesercipher but ran into issue  were there is an infinite loop with  react

--- src/components/CaeserCipher.jsx
+++ src/components/CaeserCipher.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const CaeserCipher = () => {
+  const [encode, setEncode] = useState('');
+  const [input, setInput] = useState('jinx');
+  const [shift, setShift] = useState(1);
+
+  useEffect(() => {
+    caeserCipher(input, shift);
+  }, [input, shift]);
+
+  function caeserCipher(input, shift) {
+    let answer = '';
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    for (let i = 0; i < input.length; i++) {
+      let letter = input[i];
+      let oldLetter = alphabet.indexOf(letter);
+      let newLetter = oldLetter + shift;
+      if (newLetter > 25) {
+        newLetter -= 26;
+      }
+      answer += alphabet[newLetter];
+    }
+    return answer;
+  }
+
   return <div>CaeserCipher</div>;
 };
 
